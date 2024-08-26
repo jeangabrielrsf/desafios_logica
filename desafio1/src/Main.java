eacb8dc908870c5ee41dbb0f74afb1af8cfbefc2
Jean Frusca <jeang96@gmail.com>
2024-08-25 23:21:35 -0300

    [feature] added Desafio1 - Resistores

--- desafio1/src/Main.java
+++ desafio1/src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
+    private static final String[] COLORS = {
+            "preto", "marrom", "vermelho", "laranja", "amarelo",
+            "verde", "azul", "violeta", "cinza", "branco"
+    };
+
+    public static String converterStringParaCor(String input) {
+        String valueStr = input.split(" ")[0];
+        double value = Double.parseDouble(valueStr.replaceAll("[kM]", ""));
+
+
+        if (valueStr.endsWith("k")) {
+            value *= 1000;
+        } else if (valueStr.endsWith("M")) {
+            value *= 1000000;
+        }
+
+        int ohms = (int) Math.round(value);
+
+        String ohmsStr = String.valueOf(ohms);
+        int firstDigit = Character.getNumericValue(ohmsStr.charAt(0));
+        int secondDigit = Character.getNumericValue(ohmsStr.charAt(1));
+
+
+        int multiplier = ohmsStr.length()-2;
+
+        return COLORS[firstDigit] + " " + COLORS[secondDigit] + " " + COLORS[multiplier] + " dourado";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] testCases = {
+                "47 ohms", "4.7k ohms", "1M ohms", "10 ohms", "100 ohms",
+                "220 ohms", "330 ohms", "470 ohms", "680 ohms", "1k ohms", "2M ohms"
+        };
+
+        for (String testCase : testCases) {
+            System.out.println(testCase + " : " + converterStringParaCor(testCase));
+        }
     }
 }
